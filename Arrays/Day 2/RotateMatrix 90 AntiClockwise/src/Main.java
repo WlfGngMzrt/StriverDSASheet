a06649500dff3c8aff1c76e409e4801fac20d0f3
syandan98@gmail.com <syandan98@gmail.com>
2023-07-28 17:10:14 +0530

    Strivers DSA Sheet - Rotate Matrix by 90 Degrees Clockwise

---
+++ Arrays/Day 2/RotateMatrix 90 AntiClockwise/src/Main.java
@@ -0,0 +1,89 @@
+import javax.swing.table.TableRowSorter;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        //matrix = bruteForce(matrix);
+        optimalMethod(matrix);
+        printMatrix(matrix);
+
+    }
+    public static int[][] bruteForce(int[][] matrix)
+    {
+        //TC : O(N*M)
+        //SC : O(N*M)
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dummyMatrix = new int[n][m];
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++)
+            {
+                dummyMatrix[j][n-i-1] = matrix[i][j];
+            }
+        }
+        return dummyMatrix;
+    }
+    public static void optimalMethod(int[][] matrix)
+    {
+        transpose(matrix);
+        printMatrix(matrix);
+        clockWiseRotate(matrix);
+    }
+
+    public static void clockWiseRotate(int[][] matrix)
+    {
+        for(int i = 0; i < matrix.length;i++)
+        {
+            arrayReversal(matrix[i]);
+        }
+    }
+    public static void transpose(int[][] matrix)
+    {
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = i+1; j < matrix[0].length; j++)
+            {
+                swap(i,j,matrix);
+            }
+        }
+    }
+    public static void swap(int i, int j, int[][] matrix)
+    {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+    public static void swapCols(int col, int i, int j, int[][] matrix)
+    {
+        int temp = matrix[i][col];
+        matrix[i][col] = matrix[j][col];
+        matrix[j][col] = temp;
+    }
+    public static void arrayReversal(int[] arr)
+    {
+        int s = 0;
+        int e = arr.length - 1;
+        while(s < e)
+        {
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+    public static void printMatrix(int[][] matrix)
+    {
+        for(int[] i : matrix)
+        {
+            for(int j : i)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+}
