fc77a10db37c7644c576c0eec3b6685ab89a27cf
syandan98@gmail.com <syandan98@gmail.com>
2023-08-04 03:04:16 +0530

    Commit

--- Arrays/Day 3/GridUniquePaths/src/Main.java
+++ Arrays/Day 3/GridUniquePaths/src/Main.java
@@ -2,4 +2,29 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int bruteForce(int n, int m)
+    {
+        //this is the most brute force way to find the solution
+        if(m == 0 && n == 0)
+        {
+            return 1;
+        }
+        if(m == 0 || n == 0)
+        {
+            return 0;
+        }
+        return bruteForce(n-1,m) + bruteForce(n,m-1);
+    }
+
+    public static int optimisedApproach(int n, int m, int[][] dp) {
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+        if (dp[n][m] == -1)
+        {
+            dp[n][m] = optimisedApproach(n-1,m,dp) + optimisedApproach(n,m-1,dp);
+        }
+        return dp[n][m];
+    }
 }
